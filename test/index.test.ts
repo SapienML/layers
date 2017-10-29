2c4892e0bbc8b305b9d14427b219561f12d55515
junaid <junaid1460@gmail.com>
2017-10-29 10:38:00 +0530

    Update : Added test case to test layers

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,12 @@
 import { suite, test, slow, timeout } from "mocha-typescript";
 import * as assert from "assert" ;
+import { Dense } from '@layers.implemented';
+// import { hello } from '../src/index';
 @suite class Test{
     @test testHello(){
-        
+        var d = new Dense()
+        console.log("created Dense layer ")
+        console.log("feeding Dense layer")    
+        let layer1: Dense = new Dense(); 
     }
 }
