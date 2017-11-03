6df989b5bdf847e0331de7e8c5c6e9d7f2f1d6ef
junaid <junaid1460@gmail.com>
2017-11-03 21:53:58 +0530

    removed uglify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,3 @@
-
-
-let comment = "/**\n <%= pkg.name %>:<%= pkg.version %> \n Copyright 2017 <%= pkg.author %> \n Released Under The <%= pkg.license %> License\n <%= pkg.url %> \n */";
-const uglifyConf = {
-    options: {
-      banner: comment
-    },
-    build: {
-      src: "dist/bundle.js",
-      dest: "dist/bundle.min.js"
-    }
-};
-
 const tsc =  {
     default :{
         src : ["./src/**/*ts"],
@@ -30,11 +17,10 @@ module.exports = function(grunt) {
     grunt.initConfig({
             pkg         : grunt.file.readJSON("package.json"),
             ts : tsc,
-            uglify      : uglifyConf
+
         });
 
-    // Load the plugin that provides the "uglify" task.
-    // grunt.loadNpmTasks("grunt-contrib-uglify");
+   
     //grunt typescript for transpiling typescript into javascript
     grunt.loadNpmTasks("grunt-ts");
     // Default task(s).
