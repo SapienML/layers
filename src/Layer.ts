1934bfa29f0479eca8dbcbefb45bec9bb99d5390
junaid <junaid1460@gmail.com>
2017-11-25 23:05:59 +0530

    M (src/Layer.ts) : Layers should follow functional api procedures, getOutputShape() would return output shape of a layer, so it is easy to  verify connections.

--- src/Layer.ts
+++ src/Layer.ts
@@ -1,6 +1,7 @@
-import { Tensor } from '@sapien/types'
+import { Tensor } from '@sapien/types';
+
 export abstract class Layer{
-
-    public abstract feedForward(data:Tensor):Tensor;
-
+    public abstract feedForward(data:Tensor):Tensor; // Do the math
+    public abstract getOutputShape():Array<number>; // Return shape of current layer
+    public abstract connectTo(previousLayer:Layer):boolean // Functional api to connect with another layer
 }
