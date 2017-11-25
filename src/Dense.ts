544598657b3944beea20c4c6378f45dfcd47ad5c
junaid <junaid1460@gmail.com>
2017-11-25 23:06:35 +0530

    M(src/Dense.ts) : Dummy implementation of Layer changes

--- src/Dense.ts
+++ src/Dense.ts
@@ -4,6 +4,12 @@ import { Tensor } from '@sapien/types';
 export class Dense extends Layer{
    public feedForward(d:Tensor):Tensor{
        var x = new Tensor()
-       return x;
+       return x; 
+   }
+   public getOutputShape():Array<number>{
+       return [0]; // Dummy implementation
+   }
+   public connectTo(previousLayer:Layer):boolean{
+       return true; // Dummy implementation
    }
 }
